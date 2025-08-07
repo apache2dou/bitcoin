50d50ac6918bcf4f98d5eb20c544ce4892361527
L <L@none.com>
2025-08-07 10:56:01 +0800

    替换dp之后随机算法。

--- src/rpc/cuda.cu
+++ src/rpc/cuda.cu
@@ -676,7 +676,7 @@ __global__ void rho()
             count_dp++;
             // 保存可区分点
             add_dp_to_buffer(d, s, dp_device_buffer, dp_buffer_size - 10);
-            x_ord.limb[0] = 1;
+            x_ord.limb[0] = (uint32_t)count_rho;
         }
         if ((count_rho & 0xFFFF) == 0) {
             if (*break_flag_dev)
